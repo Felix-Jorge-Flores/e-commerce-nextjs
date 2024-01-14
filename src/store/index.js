c175b8c9c6797413cfef7365f7492842f01d3e33
Felix <felixandjorge08@gmail.com>
2024-01-14 16:05:26 -0500

    Clean base project

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 export * from './auth/authSlice';
 export * from './ui/uiSlice';
-export * from './calendar/calendarSlice';
 export * from './bonillaShop/bonillaShopSlice';
 
 //El store debe exportarse ultimo
